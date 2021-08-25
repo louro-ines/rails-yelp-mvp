ed7261b21c53189bb6d0281e393dc03fd965be32
Ines Louro <inespazlouro@gmail.com>
2021-08-25 13:25:02 +0100

    finished exercise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating 5 fake restaurants...'
+dishoom = {
+  name: 'Dishoom',
+  address: '7 Boundary St, London E2 7JE',
+  phone_number: rand(10**10).to_s.rjust(10, '0'),
+  category: %w[chinese italian japanese french belgian].sample
+}
+
+pizza_east = {
+  name: 'Pizza East',
+  address: '56A Shoreditch High St, London E1 6PQ',
+  phone_number: rand(10**10).to_s.rjust(10, '0'),
+  category: %w[chinese italian japanese french belgian].sample
+}
+
+dimsum = {
+  name: 'DimSum',
+  address: 'Amoreiras, Lisboa',
+  phone_number: rand(10**10).to_s.rjust(10, '0'),
+  category: 'chinese'
+}
+
+local = {
+  name: 'Local - Your Healthy Kitchen',
+  address: 'Rua Rodrigues Sampaio, Lisboa',
+  phone_number: rand(10**10).to_s.rjust(10, '0'),
+  category: %w[chinese italian japanese french belgian].sample
+}
+
+sa_morais = {
+  name: 'Sushi dos Sá Morais',
+  address: 'Rua Castilho, Lisboa',
+  phone_number: rand(10**10).to_s.rjust(10, '0'),
+  category: 'japanese'
+}
+
+[dishoom, pizza_east, dimsum, local, sa_morais].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts 'Finished!'
